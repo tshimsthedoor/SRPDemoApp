c54929cf163d10bb741eddf9ee6c55667b91e8d8
tresor <tshims79@gmail.com>
2023-07-10 16:08:15 +0200

    I call all my method on the main method on the program class

--- SRPDemoApp/Program.cs
+++ SRPDemoApp/Program.cs
@@ -6,7 +6,35 @@ namespace SRPDemoApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("************* A demo that follows SRP.*******************");
+            Employee robin = new Employee("Robin", "Musau", 6);
+            PrintEmployeeDetail(robin);
+            PrintEmployeeId(robin);
+            PrintSeniority(robin);
+
+            Console.WriteLine("\n**********************************\n");
+
+            Employee rachel = new Employee("Rachel", "Ntumba", 8.9);
+            PrintEmployeeDetail(rachel);
+            PrintEmployeeId(rachel);
+            PrintSeniority(rachel);
+
+            Console.ReadKey();
+        }
+
+        private static void PrintSeniority(Employee robin)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void PrintEmployeeId(Employee robin)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void PrintEmployeeDetail(Employee robin)
+        {
+           robin
         }
     }
 }
