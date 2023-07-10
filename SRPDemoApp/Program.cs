1e450af9e48bf9d2c13938467cae6877531000c4
tresor <tshims79@gmail.com>
2023-07-10 15:51:46 +0200

    Add project files.

---
+++ SRPDemoApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SRPDemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
